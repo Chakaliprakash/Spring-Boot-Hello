f87dbd82541b8e96bc83a1c2aa824da3b247bfe2
Prakash Chakali <vickyprakash768@gmail.com>
2025-09-18 16:46:45 +0000

    Initial commit

--- demo/demo/src/main/java/com/example/demo/model/User.java
+++ demo/demo/src/main/java/com/example/demo/model/User.java
@@ -1,9 +1,15 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "\"user\"")  // <-- ADD THIS LINE
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -11,5 +17,20 @@ public class User {
     private String username;
     private String password;
 
-    // getters & setters
+    // Getters and setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
